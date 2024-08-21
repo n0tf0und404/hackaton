fb9f73e1ed5edd4a289eb4a67486aa1c5b0fdcab
Fabian <garciafabianemmanuel@gmail.com>
2024-08-21 14:31:48 -0300

    configuracion basica del servidor

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-import moduleName from 'module';
+import app from "./src/app.js";
+import env from "./src/enviroment/enviroment.js";
+
+
+const port = env.PORT;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
