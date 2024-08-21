361824e42b1c11438a772591c7ac2b8494ae5fe0
Fabian <garciafabianemmanuel@gmail.com>
2024-08-21 14:43:06 -0300

    middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,15 @@
 import Express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
 
 const app = Express();
+
+app.use(cors());
+app.use(helmet()),
+app.use(morgan("dev"));
+app.use(Express.json());
+
 
 export default app
