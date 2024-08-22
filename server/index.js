568031b11901bc3e6e50b17ba8a6b5b1e50cc641
EricWithC04 <negativegame117@gmail.com>
2024-08-22 20:25:44 -0300

    funcion para conectar la base de datos

--- server/index.js
+++ server/index.js
@@ -1,25 +1,12 @@
 import app from "./app.js";
 import {environments as env} from "./src/config/environments.js";
-import sequelize from "./src/config/db.js"; // Importar como default
+import { connectDB } from "./src/config/connectDB.js";
 
-sequelize
-  .authenticate()
+connectDB()
   .then(() => {
-    console.log("Database connected...");
-
-    sequelize
-      .sync({ alter: true })
-      .then(() => {
-        console.log("Database synced...");
-
-        app.listen(env.PORT || 3000, () => {
-          console.log(`Server running on port ${env.PORT || 3000}`);
-        });
-      })
-      .catch((err) => {
-        console.error("Failed to sync database:", err);
-        process.exit(1);
-      });
+    app.listen(env.PORT || 3000, () => {
+      console.log(`Server running on port ${env.PORT || 3000}`);
+    });
   })
   .catch((err) => {
     console.error("Failed to connect to the database:", err);
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,25 +2,14 @@ import { Sequelize } from "sequelize";
 import { environments } from "./environments.js";
 
 const sequelize = new Sequelize(
-  environments.DB_NAME,
-  environments.DB_USER,
-  environments.DB_PASSWORD,
+  environments.DB.DB_NAME,
+  environments.DB.DB_USER,
+  environments.DB.DB_PASSWORD,
   {
-    host: environments.DB_HOST,
-    dialect: environments.DB_DIALECT,
-    port: environments.DB_PORT,
+    host: environments.DB.DB_HOST,
+    dialect: environments.DB.DB_DIALECT,
+    port: environments.DB.DB_PORT,
   }
 );
 
 export default sequelize; // Cambiar a export default
-
-export const connectDB = async () => {
-  await sequelize
-    .sync()
-    .then(() => {
-      console.log("Connected to the database");
-    })
-    .catch((err) => {
-      console.log("Unable to connect to the database", err);
-    });
-};
