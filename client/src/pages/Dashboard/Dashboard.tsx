07e77a527cf47cf9eb3c68a65151fa4ea1248160
EricWithC04 <negativegame117@gmail.com>
2024-08-22 21:39:25 -0300

    Traer informacion del usuario desde el servidor

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react"
+
 const Dashboard = () => {
+
+    useEffect(() => {
+        const token = JSON.parse(localStorage.getItem('token') as string).token
+        
+        fetch(`http://localhost:3000/users/userByToken`, {
+            method: 'GET',
+            headers: {
+                'authorization': token,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>Dashboard</div>
     )
