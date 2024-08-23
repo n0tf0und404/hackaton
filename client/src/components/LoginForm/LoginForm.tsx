346c9897c6379114d98ba7435d14ea12f5c37e16
EricWithC04 <negativegame117@gmail.com>
2024-08-22 21:05:01 -0300

    Formulario de inicio de sesión conectado con el servidor

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,20 +1,56 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './LoginForm.module.css';
 
 const LoginForm = () => {
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        setData({ ...data, [name]: value });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        fetch('http://localhost:3000/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                localStorage.setItem('token', JSON.stringify(data.token));
+                navigate('/dashboard')
+            })
+            .catch(err => console.log(err));
+    };
+
     return (
-        <form className={styles['main-form']}>
+        <form className={styles['main-form']} onSubmit={handleSubmit}>
             <h3>Iniciar Sesión</h3>
             <div className={styles['form-inputs']}>
                 <input
                     name="email"
                     type="email"
                     placeholder="Email"
+                    onChange={handleChange}
+                    value={data.email}
                     autoComplete="off"
                 />
                 <input
                     name="password"
                     type="password"
                     placeholder="Contraseña"
+                    onChange={handleChange}
+                    value={data.password}
                     autoComplete="off"
                 />
             </div>
